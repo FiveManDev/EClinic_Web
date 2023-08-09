c1c2fc807d3ed8f2a883f24ab66472db366bc86c
SangHa <haphuocsang1801@gmail.com>
2023-08-09 19:25:29 +0700

    fix bug

--- src/module/Admin/Account/Patient.tsx
+++ src/module/Admin/Account/Patient.tsx
@@ -1,10 +1,6 @@
 import MainHeadingLayout from "layout/Management/MainHeadingLayout"
-import TabsCustom from "layout/Management/components/TabsCustom"
-import CreateAccount from "module/Doctor/components/CreateAccount"
-import ListDoctor from "module/Doctor/components/ListDoctor"
 import ListPatient from "module/Doctor/components/ListPatient"
 import Head from "next/head"
-import React, { useMemo } from "react"
 
 const Patient = () => {
   return (
